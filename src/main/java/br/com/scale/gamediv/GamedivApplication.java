96ea49f508de07b234435d7493146ae5eb32c8d4
João Marcos <joaomarcossm2013@hotmail.com>
2021-06-16 16:31:36 -0300

    Init project with spring IO, with H2 DB, Spring Dev Tools, Spring Web and Thymeleaf

---
+++ src/main/java/br/com/scale/gamediv/GamedivApplication.java
@@ -0,0 +1,13 @@
+package br.com.scale.gamediv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamedivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamedivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/scale/gamediv/GamedivApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.scale.gamediv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamedivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
