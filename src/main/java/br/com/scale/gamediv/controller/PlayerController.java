50533a1de93994bce28293cf5e7405e11f37d195
João Marcos <joaomarcossm2013@hotmail.com>
2021-06-19 01:39:53 -0300

    Create playerservice for separate rules of controller and implements select all and select by id for players

--- src/main/java/br/com/scale/gamediv/controller/PlayerController.java
+++ src/main/java/br/com/scale/gamediv/controller/PlayerController.java
@@ -3,19 +3,34 @@ package br.com.scale.gamediv.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.scale.gamediv.model.Player;
+import br.com.scale.gamediv.service.PlayerService;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping(value="/players")
 public class PlayerController {
+
+    @Autowired
+    private PlayerService service;
     
     @GetMapping
-    public ResponseEntity<Player> findAll() {
-        Player p = new Player(1L, "João");
-        return ResponseEntity.ok().body(p);
+    public ResponseEntity<List<Player>> findAll() {
+       List<Player> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
         
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Player> findById(@PathVariable Long id) {
+        Player obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/br/com/scale/gamediv/service/PlayerService.java
@@ -0,0 +1,26 @@
+package br.com.scale.gamediv.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.scale.gamediv.model.Player;
+import br.com.scale.gamediv.repository.PlayerRepository;
+
+@Service
+public class PlayerService {
+
+    @Autowired
+    private PlayerRepository repository;
+
+    public List<Player> findAll() {
+        return repository.findAll();
+    }
+    
+    public Player findById(Long id) {
+        Optional<Player> obj = repository.findById(id);
+        return obj.get();
+    }
+}
