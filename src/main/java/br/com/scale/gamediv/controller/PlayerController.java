32799e358a564cec049fcea8dc1767cf1b0a863d
João Marcos <joaomarcossm2013@hotmail.com>
2021-06-20 00:29:27 -0300

    update implemented with method put

--- src/main/java/br/com/scale/gamediv/controller/PlayerController.java
+++ src/main/java/br/com/scale/gamediv/controller/PlayerController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -51,4 +52,11 @@ public class PlayerController {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Player> update(@PathVariable Long id, @RequestBody Player obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
