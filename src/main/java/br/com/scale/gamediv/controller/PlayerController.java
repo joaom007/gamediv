8e8937f63a09cfeb7658765aed75837cf8152043
João Marcos <joaomarcossm2013@hotmail.com>
2021-06-20 00:09:26 -0300

    implements insert with post with cod 201 and end point return

--- src/main/java/br/com/scale/gamediv/controller/PlayerController.java
+++ src/main/java/br/com/scale/gamediv/controller/PlayerController.java
@@ -1,16 +1,20 @@
 package br.com.scale.gamediv.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.scale.gamediv.model.Player;
 import br.com.scale.gamediv.service.PlayerService;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -25,12 +29,19 @@ public class PlayerController {
        List<Player> list = service.findAll();
 
         return ResponseEntity.ok().body(list);
-        
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Player> findById(@PathVariable Long id) {
         Player obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    //return cod 201 created and end point get in header
+    @PostMapping
+    public ResponseEntity<Player> insert(@RequestBody Player obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
